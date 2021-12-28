be1a0448587ea675dde07422ac8bdcfdc8e72a75
Everett <85105218+Stuxe@users.noreply.github.com>
2021-12-28 14:45:38 -0900

    Update GetADGroupMember.ps1

--- GetADGroupMember.ps1
+++ GetADGroupMember.ps1
@@ -1,1 +1,2 @@
+# Use this script to get a list of Users in a specific security group and export the list to a csv file. 
 Get-ADGroupMember -identity “Security Group” | select name | Export-csv -path C:\it\filename.csv -NoTypeInformation
